9f77b2f5d1b9a9e0b93773edb629a61ef26b6806
Danica Duan <d.duan20@imperial.ac.uk>
2020-10-10 20:21:41 +0100

    Updated 10/10/2020

--- Week1/code/ConcatenateTwoFiles.sh
+++ Week1/code/ConcatenateTwoFiles.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-cat $1 > $3
-cat $2 >> $3
-echo "merged file is"
-cat $3
-
+if [-z "$1"]
+then echo "Please input the first file"
+elif [-z "$2"]
+then echo "Please input the second file"
+else  
+	cat $1 > $3
+	cat $2 >> $3
+	echo "merged file is"
+	cat $3
+fi
