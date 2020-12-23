f8dd498b96e8c6c167fae3a9d188cc92139e113e
DaniDuan <d.duan20@imperial.ac.uk>
2020-12-23 15:05:06 +0000

    Added mres research proposal

---
+++ Research Proposal/Gantt chart.R
@@ -0,0 +1,17 @@
+library(reshape2)
+library(ggplot2)
+
+tasks = c("Literature Review", "Refining Project Design", "Code Writing", "Preliminary Simulations\n & Code Refining", "Model Simulation", "Result Analysis", "Thesis Writing")
+tasks = factor(tasks, levels = tasks)
+start.date = as.Date(c("2021-01-10", "2021-01-10", "2021-01-20","2021-02-15", "2021-03-15", "2021-06-15", "2021-03-15"))
+end.date = as.Date(c("2021-02-10", "2021-03-15","2021-02-15", "2021-03-15", "2021-06-15", "2021-08-01", "2021-09-15"))
+chart = data.frame(tasks, start.date,end.date)
+chart = melt(chart, measure.vars = c("start.date", "end.date"))
+pdf("Gantt_chart.pdf", width = 9, height = 3)
+ggplot(chart, aes(value, tasks)) + 
+  geom_line(size = 6) + theme_bw()+
+  scale_x_date(date_labels = "%b %Y",date_breaks = "1 months") +
+  xlab(NULL) +
+  ylab(NULL)
+graphics.off()
+
